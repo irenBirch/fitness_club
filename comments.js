a845fec03fa312d49ea94d02939574244eed9b8c
irenBirch <iraberezka2005@gmail.com>
2025-04-28 00:34:28 +0300

    full lab7

--- comments.js
+++ comments.js
@@ -2,3 +2,13 @@ setTimeout(() => {
   location.href =
     "https://top20.ua/kyiv/sport-krasota/fitnes-tsentryi/sport-life-set-fitnes-klubov.html";
 }, 15000);
+
+document.addEventListener("click", function (event) {
+  const id = event.target.dataset.toggleId;
+  if (!id) return; 
+
+  const elem = document.getElementById(id);
+  if (!elem) return; 
+
+  elem.hidden = !elem.hidden; 
+});
